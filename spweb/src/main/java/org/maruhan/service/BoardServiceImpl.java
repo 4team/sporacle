b4b7f5fb2b0c5d86a0715a2bf28f6cb0e17d008a
maruhan <didhddldlq@naver.com>
2015-12-03 13:21:42 +0900

    DAO Test Success.

--- spweb/src/main/java/org/maruhan/service/BoardServiceImpl.java
+++ spweb/src/main/java/org/maruhan/service/BoardServiceImpl.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.maruhan.domain.BoardVO;
 import org.maruhan.persistence.AbstractCRUDMapper;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class BoardServiceImpl extends AbstractCRUDMapper<BoardVO, Integer> implements BoardService {
 
 	@Override
