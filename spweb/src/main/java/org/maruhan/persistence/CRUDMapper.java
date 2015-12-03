bd6847e64c3363f26411c911af74ca667efd5958
maruhan <didhddldlq@naver.com>
2015-12-03 12:41:59 +0900

    많은 것이 바뀌었어요. 12:41

---
+++ spweb/src/main/java/org/maruhan/persistence/CRUDMapper.java
@@ -0,0 +1,8 @@
+package org.maruhan.persistence;
+
+public interface CRUDMapper<V,K> {
+	public void create(V vo) throws Exception;
+	public V read(K key) throws Exception;
+	public void delete(K key) throws Exception;
+	public void update(V vo) throws Exception;
+}
