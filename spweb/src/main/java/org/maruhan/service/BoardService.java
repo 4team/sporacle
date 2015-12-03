e143c1297777897a386ac322e9cf08d377f8dda8
maruhan <didhddldlq@naver.com>
2015-12-03 14:53:10 +0900

    으아니 또 수정해야한다니!

--- spweb/src/main/java/org/maruhan/service/BoardService.java
+++ spweb/src/main/java/org/maruhan/service/BoardService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.maruhan.domain.BoardVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class BoardService {
 
 	@Autowired
