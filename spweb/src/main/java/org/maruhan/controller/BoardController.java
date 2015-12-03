6ce9f21a6a3543f03cc6e6f7ea23f3b3b0b51871
islejeon <isle.jeon@gmail.com>
2015-12-03 12:26:52 +0900

    선생님 코드로 수정 및 추가

--- spweb/src/main/java/org/maruhan/controller/BoardController.java
+++ spweb/src/main/java/org/maruhan/controller/BoardController.java
@@ -1,9 +1,13 @@
 package org.maruhan.controller;
 
+import org.maruhan.service.BoardService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/board/*")
@@ -11,9 +15,15 @@ public class BoardController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
 	
-	@RequestMapping("/list")
-	public void listAll() throws Exception{
+	//@Autowired
+	//private BoardService service;
+	
+	
+	@RequestMapping(value = "/list" , method= RequestMethod.GET)
+	public void listAll(Model model) throws Exception{
 		logger.info("BoardController........ info");
+		
+		//model.addAttribute("list", service.listAll());
 	}
 
 }
